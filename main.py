7738351cffff948b3a935c09f7f6e84c7606c35a
Alexhappytim <timoshkovalex@gmail.com>
2024-12-24 19:32:25 +0700

    Задача десант

--- main.py
+++ main.py
@@ -1,6 +1,77 @@
-x1, r1, y1 = list(map(int, input().split()))
-x2, r2, y2 = list(map(int, input().split()))
-if (x1 - x2) ** 2 + (y1 - y2) ** 2 > (r2 + r1) ** 2:
-    print('NO')
-else:
-    print('YES')
+import pygame
+import os
+import sys
+import random
+from pygame import Color
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((1, 1))
+        image.set_colorkey(colorkey)
+    return image
+
+class Mountain(pygame.sprite.Sprite):
+    image = load_image("mountains.png")
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Mountain.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        # располагаем горы внизу
+        self.rect.bottom = height
+class Landing(pygame.sprite.Sprite):
+    image = load_image("pt.png")
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        if not pygame.sprite.collide_mask(self, mountain):
+            self.rect = self.rect.move(0, 1)
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('shaders')
+
+    size = width, height = 789, 500
+    screen = pygame.display.set_mode(size)
+
+    all_sprites = pygame.sprite.Group()
+
+    clock = pygame.time.Clock()
+    pygame.mouse.set_visible(True)
+    running = True
+
+
+    mountain = Mountain()
+
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Landing(event.pos)
+
+
+        all_sprites.update()
+        all_sprites.draw(screen)
+        pygame.display.flip()
+        screen.fill((0, 0, 0))
+        clock.tick(100000)
+    pygame.display.flip()
